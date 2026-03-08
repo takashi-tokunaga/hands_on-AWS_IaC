3a7fe504b8d087c9487c0d546333828112fbd789
takashi_tokunaga <takashi.tokunaga.ibaraki@gmail.com>
2026-03-08 13:36:48 +0900

    terraform fmt ./vpc.tf

--- terrafrom/vpc.tf
+++ terrafrom/vpc.tf
@@ -7,9 +7,9 @@ resource "aws_vpc" "vpc" {
 }
 
 resource "aws_subnet" "subnet_1a" {
-  vpc_id = aws_vpc.vpc.id
-  cidr_block = "10.20.1.0/24"
-  availability_zone = "ap-northeast-1a"
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = "10.20.1.0/24"
+  availability_zone       = "ap-northeast-1a"
   map_public_ip_on_launch = true
 
   tags = {
@@ -19,9 +19,9 @@ resource "aws_subnet" "subnet_1a" {
 
 
 resource "aws_subnet" "subnet_1c" {
-  vpc_id = aws_vpc.vpc.id
-  cidr_block = "10.20.2.0/24"
-  availability_zone = "ap-northeast-1c"
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = "10.20.2.0/24"
+  availability_zone       = "ap-northeast-1c"
   map_public_ip_on_launch = true
 
   tags = {
@@ -33,7 +33,7 @@ resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.vpc.id
 
   tags = {
-    Name ="test-iac-igw"
+    Name = "test-iac-igw"
   }
 }
 
@@ -46,17 +46,17 @@ resource "aws_route_table" "public" {
 }
 
 resource "aws_route" "public" {
-  route_table_id = aws_route_table.public.id
+  route_table_id         = aws_route_table.public.id
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id = aws_internet_gateway.igw.id
+  gateway_id             = aws_internet_gateway.igw.id
 }
 
 resource "aws_route_table_association" "public_1a" {
-  subnet_id = aws_subnet.subnet_1a.id
+  subnet_id      = aws_subnet.subnet_1a.id
   route_table_id = aws_route_table.public.id
 }
 
 resource "aws_route_table_association" "public_1c" {
-  subnet_id = aws_subnet.subnet_1c.id
+  subnet_id      = aws_subnet.subnet_1c.id
   route_table_id = aws_route_table.public.id
 }
