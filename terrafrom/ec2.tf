e792f4f491f3af9778573d3ba64bb76880eb0511
takashi_tokunaga <takashi.tokunaga.ibaraki@gmail.com>
2026-03-08 15:41:02 +0900

    add create ec2 instance

--- terrafrom/ec2.tf
+++ terrafrom/ec2.tf
@@ -39,3 +39,15 @@ resource "aws_security_group_rule" "outbound" {
   protocol          = "-1"
   cidr_blocks       = ["0.0.0.0/0"]
 }
+
+resource "aws_instance" "ec2" {
+  ami                    = "ami-081a019e7591fd4af"
+  instance_type          = "t3.micro"
+  subnet_id              = aws_subnet.subnet_1a.id
+  vpc_security_group_ids = [aws_security_group.ec2.id]
+  key_name               = "iac_instance"
+
+  tags = {
+    Name = "test-iac-ec2"
+  }
+}
